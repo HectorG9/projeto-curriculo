b7d7542e078acc142117faa18426434180f04d41
Hector <hector.guedesmonteiro@gmail.com>
2024-06-19 17:22:53 -0300

    first commit

---
+++ curriculo/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="" />
+      <strong>Curriculo Elêtronico</strong>
+    </header>
+  )
+}
