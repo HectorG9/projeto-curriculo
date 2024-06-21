e342c89869c8499b221033e279d9bd6216711a37
Hector <hector.guedesmonteiro@gmail.com>
2024-06-21 16:49:07 -0300

    Adicionei um sidebar e criei o post

--- curriculo/src/components/Post.jsx
+++ curriculo/src/components/Post.jsx
@@ -1,8 +1,31 @@
 
 export default function Post() {
   return (
-    <div>
-      
-    </div>
+    <article>
+      <header>
+        <div>
+          <img src="https://cdn-icons-png.flaticon.com/256/7967/7967000.png" alt="" />
+          <div>
+            <strong>Hector Grôppo G. Monteiro</strong>
+            <span>Programador Frontend</span>
+            <span>Automação em Python</span>
+          </div>
+        </div>
+      </header>
+      <div>
+        <p>Sobre Mim</p> {/* sobre mim */}
+        <p>Estudei remotamente por conta própria, cursos sobre linguagem de programação python, e desenvolvimento full stack </p>
+        <p>Formado profissionalmente através do senai no curso de desenvolvimento frontend</p> 
+        <p>Calistenico, disciplinado, gosto de exatas, livros, games e tecnologia no geral</p>
+        <p>Link Repositórios</p>
+        <a href="https://github.com/HectorG9">Repositório1</a>
+        <a href="https://github.com/hectorgroppo">Repositório2</a>
+        <p>Ultimo Projeto</p>
+        <a href="https://replicando-site-14x42jb5z-hectorg9s-projects.vercel.app/">Projeto Netflix, em andamento</a>
+      </div>
+      <form action="">
+        <strong>Experiência Profissional</strong>
+      </form>
+    </article>
   )
 }
--- curriculo/src/components/Sidebar.jsx
+++ curriculo/src/components/Sidebar.jsx
@@ -1,8 +1,20 @@
+// Importando o Style
+import style from './sidebar.module.css';
+
 // criando sidebar
 export default function Sidebar() {
   return (
-    <div>
-      <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab obcaecati dolor nemo eligendi? Blanditiis consequatur temporibus debitis ratione minima alias non veritatis quas veniam velit, aut, maxime mollitia repellat consequuntur?</h1>
-    </div>
+     <aside className={style.sidebar}> {/*configuração para todo o aside/sidebar*/}
+      <img className={style.cover} src="https://blog.hubdodesenvolvedor.com.br/wp-content/uploads/2024/03/capa-linguagens-em-alta.jpg" alt="" />
+      <div className={style.profile}>
+        <img className={style.avatar} src="https://cdn-icons-png.flaticon.com/256/7967/7967000.png" alt="" />
+        <strong>Hector Grôppo G. Monteiro</strong>
+        <span>Desenvolvimento Front End</span>
+        <span>Análise de dados com Python</span>
+      </div>
+      <footer>
+        <a href="https://github.com/HectorG9">Meu Github</a>
+      </footer>
+    </aside>
   )
 }
