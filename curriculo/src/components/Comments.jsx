673e2b5eba751b9f3b38043acaaffa82507682e3
Hector <hector.guedesmonteiro@gmail.com>
2024-06-24 14:29:48 -0300

    adicionei uma imagem ao "Experiencia1"

--- curriculo/src/components/Comments.jsx
+++ curriculo/src/components/Comments.jsx
@@ -1,20 +1,20 @@
 // importando o style
 import style from './comments.module.css'; 
+
 // criando o comment
 export default function Comments() {
   return (
     <div className={style.comments}>
-      <img src="" alt="profile author" />
+      <img src="https://miro.medium.com/v2/resize:fit:1400/0*9wl_4W5IAemkH3RC.jpg" alt="profile author" />
       <div className={style.commentsContent}>
-            <header>
-                <div>
-                    <strong>Experiência 1</strong>
-                </div>
-            </header>
+          <header>
+              <div>
+                  <strong>Experiência 1</strong>
+              </div>
+          </header>
          <p>Experiência em criação de jogo no pycharm utilizando a biblioteca pygames</p> {/* insira sua experiência */}
          <p>Experiência em criação de projetos como este</p>
-
-        </div>
+      </div>
     </div>
     
   );
